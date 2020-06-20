4b4bc7b532764a13ce931ce57c8842b5aa14e433
Aadi <aadiamazing5@gmail.com>
2020-06-20 14:10:12 -0400

    added footer and contact, added img assets, updated js file and added minor section updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,21 @@
 (function($) {
     "use strict";
 
+    // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
     // Initiate the wowjs animation library
   new WOW().init();
 
